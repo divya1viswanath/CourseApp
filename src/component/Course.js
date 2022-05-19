c0645649970ec7491fa9a1aabbe8388491e02c5a
divya1viswanath <divyarugmini798@gmail.com>
2022-05-19 11:49:24 +0530

    header

--- src/component/Course.js
+++ src/component/Course.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import View from './Header'
+import Navbar from './Navbar'
 
 const Course = () => {
     var [Ctitle,setCtitle]=useState("")
@@ -14,7 +14,7 @@ const subData =() => {
 }
   return (
     <div>
-        
+        <Navbar/>
          <div className="container">
             <div className="row">                                                                              
                 <div className="col col-12 col-sm-12 col-md-2 col-lg-12 col-xl-12 col-xxl-12">
---
+++ src/component/Courseview.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Courseview = () => {
+  return (
+    <div>Courseview</div>
+  )
+}
+
+export default Courseview
